51032ffb1c15818838b68dd11d136510e46768dc
Konstantinos Konstantinidis <dinos66@yahoo.com>
2016-12-13 18:14:49 +0200

    dependency adjust for 32bit

--- dependencyCheck.py
+++ dependencyCheck.py
@@ -8,7 +8,7 @@
 # Copyright:     (c) ITI (CERTH) 2016
 # Licence:       <apache licence 2.0>
 #-------------------------------------------------------------------------------
-import subprocess
+import subprocess, platform
 print('Running dependency checks...')
 try:
     from setuptools import setup
@@ -16,10 +16,14 @@ except:
     subprocess.call('python ez_setup.py')
     pass    
 subprocess.call('easy_install -U pip')
-subprocess.call('pip install numpy-1.11.2+mkl-cp35-cp35m-win_amd64.whl')
-subprocess.call('pip install matplotlib-1.5.3-cp35-cp35m-win_amd64.whl')
-subprocess.call('pip install pandas-0.19.1-cp35-cp35m-win_amd64.whl')
-subprocess.call('pip install scipy-0.18.1-cp35-cp35m-win_amd64.whl')
+if platform.architecture()[0] == '64bit':
+    extension = 'win_amd64'
+else:
+    extension = 'win32'
+subprocess.call('pip install numpy-1.11.2+mkl-cp35-cp35m-'+extension+'.whl')
+subprocess.call('pip install matplotlib-1.5.3-cp35-cp35m-'+extension+'.whl')
+subprocess.call('pip install scipy-0.18.1-cp35-cp35m-'+extension+'.whl')
+subprocess.call('pip install pandas-0.19.1-cp35-cp35m-'+extension+'.whl')
 setup(
     name="somocluGUI",
     description='somoclu GUI wrapper for the PERICLES project',
